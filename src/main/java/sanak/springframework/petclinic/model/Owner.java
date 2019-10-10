00b9b68439de99c36d935fa552ba8ae7e7420187
Oleksii Sanak <sanak.alex@gmail.com>
2019-10-10 19:29:22 +0300

    Refactored to multi module maven project. Closes #1.

--- src/main/java/sanak/springframework/petclinic/model/Owner.java
+++
@@ -1,5 +0,0 @@
-package sanak.springframework.petclinic.model;
-
-public class Owner extends Person {
-
-}
--- src/main/java/sanak/springframework/petclinic/model/Vet.java
+++
@@ -1,4 +0,0 @@
-package sanak.springframework.petclinic.model;
-
-public class Vet extends Person {
-}
