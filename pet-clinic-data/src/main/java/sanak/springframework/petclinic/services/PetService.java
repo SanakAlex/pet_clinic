5a72c1d574a193f16888eb458743462808db92ab
Oleksii Sanak <sanak.alex@gmail.com>
2019-10-16 01:01:32 +0300

    Closes #11. Created Common CrudService

--- pet-clinic-data/src/main/java/sanak/springframework/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/sanak/springframework/petclinic/services/PetService.java
@@ -2,13 +2,5 @@ package sanak.springframework.petclinic.services;
 
 import sanak.springframework.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-  Pet findById(Long id);
-
-  Pet save(Pet pet);
-
-  Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/sanak/springframework/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/sanak/springframework/petclinic/services/VetService.java
@@ -2,13 +2,5 @@ package sanak.springframework.petclinic.services;
 
 import sanak.springframework.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-  Vet findById(Long id);
-
-  Vet save(Vet pet);
-
-  Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
