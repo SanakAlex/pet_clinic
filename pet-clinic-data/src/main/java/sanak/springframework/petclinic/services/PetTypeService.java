65ba24d6d4ea118a9ca77d3c61206972d2b7a412
Oleksii Sanak <sanak.alex@gmail.com>
2020-02-20 01:32:22 +0200

    Added pet type map service. Closes #29

---
+++ pet-clinic-data/src/main/java/sanak/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package sanak.springframework.petclinic.services;
+
+import sanak.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
