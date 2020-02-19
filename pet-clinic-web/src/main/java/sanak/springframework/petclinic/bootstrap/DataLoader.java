b6ffb75b0e688911153103f33bd103cecc2320c3
Oleksii Sanak <sanak.alex@gmail.com>
2020-02-20 01:48:52 +0200

    Added pet types to bootstrap. Closes #32

--- pet-clinic-web/src/main/java/sanak/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/sanak/springframework/petclinic/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package sanak.springframework.petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import sanak.springframework.petclinic.model.Owner;
+import sanak.springframework.petclinic.model.PetType;
 import sanak.springframework.petclinic.model.Vet;
 import sanak.springframework.petclinic.services.OwnerService;
+import sanak.springframework.petclinic.services.PetTypeService;
 import sanak.springframework.petclinic.services.VetService;
 
 @Component
@@ -12,10 +14,13 @@ public class DataLoader implements CommandLineRunner {
 
   private final OwnerService ownerService;
   private final VetService vetService;
+  private final PetTypeService petTypeService;
 
-  public DataLoader(OwnerService ownerService, VetService vetService) {
+  public DataLoader(OwnerService ownerService, VetService vetService,
+      PetTypeService petTypeService) {
     this.ownerService = ownerService;
     this.vetService = vetService;
+    this.petTypeService = petTypeService;
   }
 
   @Override
@@ -40,5 +45,15 @@ public class DataLoader implements CommandLineRunner {
     vet2.setLastName("Snow");
     vetService.save(vet2);
     System.out.println("Loaded vets");
+
+    PetType dog = new PetType();
+    dog.setName("Dog");
+    PetType savedDogPetType = petTypeService.save(dog);
+    PetType cat = new PetType();
+    cat.setName("Dog");
+    PetType savedCatPetType = petTypeService.save(cat);
+    System.out.println("Loaded pet types");
+
+
   }
 }
