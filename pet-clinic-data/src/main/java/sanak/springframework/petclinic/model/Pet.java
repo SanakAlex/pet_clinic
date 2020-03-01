c88ed9ca4d21612a177c3a43767fc40c3c033c82
Oleksii Sanak <sanak.alex@gmail.com>
2020-03-01 14:21:27 +0200

    added Owners entity JPA implementation. Closes #36

--- pet-clinic-data/src/main/java/sanak/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/sanak/springframework/petclinic/model/Pet.java
@@ -1,12 +1,25 @@
 package sanak.springframework.petclinic.model;
 
 import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+  @Column(name = "name")
   private String name;
+  @ManyToOne
+  @JoinColumn(name = "pet_type_id")
   private PetType petType;
+  @ManyToOne
+  @JoinColumn(name = "owner_id")
   private Owner owner;
+  @Column(name = "birth_day")
   private LocalDate birthDate;
 
   public String getName() {
